35fdf8e405db36700cb716f81bd180255f7c22db
JOÃO VITOR NERES TEIXEIRA <joao.neres@portalsesisp.org.br>
2026-02-12 16:14:58 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome = "João Neres";
+int anoNascimentoMeu = 2010;
+decimal peso = 57.4m;
+
+Console.Clear();
+Console.WriteLine($"Meu nome é {nome}.");
+Console.WriteLine($"Meu nascimento foi em {anoNascimentoMeu}");
+Console.WriteLine($"Meu peso é {peso}");
